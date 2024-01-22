06829640e099abefea70e1d9feb34d4538583792
tom tsai <devtsai@outlook.com>
2024-01-22 15:49:02 +0800

    stash 评语工具

---
+++ test/piyue.js
@@ -0,0 +1,41 @@
+window.onload = function () {
+  (function () {
+    "use strict";
+    // 创建一个div，居中显示，宽高800*600，再所有元素的前面，紫色背景
+    // var div = document.createElement("div");
+    // div.style.cssText =
+    //   "position:fixed;right:0;top:0;width:30%;height:100%;z-index:999999;background-color:rgba(255,0,255,0.5);";
+    // // insert div to body
+    // document.body.insertBefore(div, document.body.firstChild);
+    // // put a table in that div
+    // var table = document.createElement("table");
+    // table.style.cssText = "width:100%;height:100%;";
+    // div.appendChild(table);
+    // // put a tr in that table
+    // var tr = document.createElement("tr");
+    // table.appendChild(tr);
+    // // put a td in that tr
+    // var td = document.createElement("td");
+    // td.style.cssText = "vertical-align:top;";
+    // tr.appendChild(td);
+    // // put a textarea in that td
+    // var textarea = document.createElement("textarea");
+    // textarea.style.cssText = "width:100%;height:100%;";
+    // td.appendChild(textarea);
+    // // put a button in that td
+    // var button = document.createElement("button");
+    // button.style.cssText = "width:100%;";
+    // button.innerHTML = "执行";
+    // td.appendChild(button);
+    // // put a td in that tr
+    // var td = document.createElement("td");
+    // td.style.cssText = "vertical-align:top;";
+    // tr.appendChild(td);
+    // // put a textarea in that td
+    // var textarea = document.createElement("textarea");
+    // textarea.style.cssText = "width:100%;height:100%;";
+    // td.appendChild(textarea);
+    // put a button in that td
+    
+  })();
+};
